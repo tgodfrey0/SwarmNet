aeb54370448a9e931154140f414210399a6ceae9
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-11-09 17:15:52 +0000

    working discovery

--- examples/discovery_example.py
+++ examples/discovery_example.py
@@ -1,4 +1,5 @@
-from swarmnet.discovery import spawn_wifi_ap
+import swarmnet.discovery as discovery
 
 if __name__=="__main__":
-  spawn_wifi_ap()
+  while(True):
+    discovery.discover_swarm_devices("O")
--- swarmnet/discovery.py
+++ swarmnet/discovery.py
@@ -1,12 +1,44 @@
 import bluetooth
 from swarmnet.logger import Logger
 
-def spawn_wifi_ap() -> None:
-  logger = Logger("discovery")
-  logger.info_header("Searching for nearby bluetooth devices")
-  
-  nearby_devices = bluetooth.discover_devices(lookup_names=True)
-  logger.success("Found {} devices.".format(len(nearby_devices)))
+logger = Logger("discovery")
 
-  for addr, name in nearby_devices:
-    logger.info("{} - {}".format(addr, name))
+def _discover_bt_devices() -> [(str, str)]:
+  logger.info("Searching for all nearby bluetooth devices")
+  
+  nearby_devices = bluetooth.discover_devices(lookup_names = True, lookup_class = False)
+  n_devices = len(nearby_devices)
+  if n_devices == 1:
+    logger.success(f"Found 1 device within range")
+  elif n_devices > 1:
+    logger.success(f"Found {n_devices} device within range")
+  else:
+    logger.warn(f"Found 0 devices within range")
+
+  # for addr, name in nearby_devices:
+  #   logger.info("{} - {}".format(addr, name))
+    
+  return nearby_devices
+    
+def discover_swarm_devices(swarm_prefix: str) -> {str, str}:
+  logger.info_header("Searching for swarm members")
+  
+  all_devices: [(str, str)] = _discover_bt_devices()
+  
+  swarm_devices: {str, str} = {}
+  prefix_length = len(swarm_prefix)
+  
+  for addr, name in all_devices:
+    if name[:prefix_length] == swarm_prefix:
+      swarm_devices[name] = addr
+      logger.info(f"Found {name} at {addr}")
+      
+  n_swarm_devices = len(swarm_devices)
+  if n_swarm_devices == 1:
+    logger.success(f"Found 1 swarm member (swarm prefix: {swarm_prefix})")
+  elif n_swarm_devices > 1:
+    logger.success(f"Found {n_swarm_devices} swarm members (swarm prefix: {swarm_prefix})")
+  else:
+    logger.warn(f"Found 0 swarm members (swarm prefix: {swarm_prefix})")
+    
+  return swarm_devices
