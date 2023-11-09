d777a95011ca87eac360321fe392fc8c5c384ddb
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-11-09 17:35:45 +0000

    structure

--- swarmnet/controller.py
+++ swarmnet/controller.py
@@ -5,8 +5,11 @@ import discovery
 
 logger = logger.Logger("Controller")
 
-class SwarmNet:
+class SwarmNet(threading.Thread):
   def __init__(self):
+    pass
+  
+  def start_communication_framework():
     pass
   
   def set_log_level(l: logger.Logger.Log_Level):
