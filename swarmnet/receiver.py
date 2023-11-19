1a209fd740e5083102b2e32a12caa4547b3ebcbc
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-11-19 14:56:11 +0000

    prints connected addr

--- swarmnet/receiver.py
+++ swarmnet/receiver.py
@@ -7,28 +7,16 @@ import threading
 log = logger.Logger("receiver")
 
 class Receiver:
-  # class Handler(socketserver.StreamRequestHandler):
-  #   def handle(self):
-  #     msg = self.rfile.readline().strip().decode("utf-8")
-  #     logger.info(f"Message received: {msg}")
-  #     parts = msg.split(":", 1)
-  #     if not self.received(parts[0]):
-  #       super.rx.put(parts[1])
-  #       super.register_received(parts[0])
-  #       super.tx.put(msg) # Gossip protocol
-      
   def __init__(self, addr: str, port: int, received: Callable[[str], bool], register_received: Callable[[str], None], rx_queue: queue.Queue, tx_queue: queue.Queue):
     self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.listener.bind((addr, port))
     self.listener.listen()
     self.listener.settimeout(5)
-    # self.listener = socketserver.TCPServer((addr, port), self.Handler)
     self.received = received
     self.register_received = register_received
     self.rx = rx_queue
     self.tx = tx_queue
   
-    # threading.Thread(target=self.listener.serve_forever)
     log.info("Waiting for connections")
     log.success("SwarmNet receiver started")
     
@@ -38,11 +26,11 @@ class Receiver:
     
   def accept_connection(self) -> None:
     try:
-      client_sock, client_info = self.listener.accept() # This waits indefinitely 
+      client_sock, client_info = self.listener.accept()
     except TimeoutError:
       return
     
-    log.info(f"Connection received")
+    log.info(f"Connection received from {client_sock.getsockname()[0]}")
 
     full_data: str = ""
     try:
