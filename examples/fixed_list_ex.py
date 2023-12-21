d0e8fefe970d94045013fe5cf0839a3786cfc7e4
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-12-21 13:59:32 +0000

    exampel

--- examples/fixed_list_ex.py
+++ examples/fixed_list_ex.py
@@ -25,10 +25,10 @@ def text_recv(msg: Optional[str]) -> None:
   print(f"Message received: {msg}")
 
 if __name__=="__main__":
-  ctrl = swarmnet.SwarmNet({"TEXT": text_recv}, device_list=[("192.168.0.120", 51000), ("192.168.0.121", 51000)])
+  ctrl = swarmnet.SwarmNet({"TEXT": text_recv, "READY": ready_recv}, device_list=[("192.168.0.120", 51000), ("192.168.0.121", 51000)])
   ctrl.start()
   
   while(get_cont()):
-    pass
+    ctrl.send("READY")
   
   ctrl.send("TEXT hello world")
